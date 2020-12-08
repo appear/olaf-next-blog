9509680883fe8e92afdc0eebd6005660c65c50fd
olaf-snow <olaf.snow@navercorp.com>
2020-12-08 19:05:27 +0900

    :hammer: ssr 시 styled-component 깨지는 문제 수정

--- .babel.config.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-    const presets = ["next/babel"]
-    const plugins = [["styled-components", { "ssr": true }]]
-  
-    return {
-        presets,
-        plugins
-    }
-}
